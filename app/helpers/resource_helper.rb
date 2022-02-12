8a5050a542fec3836eb778c6650fb0f3a5ed8a8e
David Singer <david@ramaboo.com>
2022-02-12 07:05:10 -0800

    better icons

--- app/helpers/resource_helper.rb
+++ app/helpers/resource_helper.rb
@@ -13,6 +13,12 @@ module ResourceHelper
     end
   end
 
+  def header_admin_delete_button(resource)
+    if policy([:admin, resource]).destroy?
+      link_to(bootstrap_icon("trash-fill"), [:admin, resource], data: {turbo_confirm: "Are you sure?", turbo_method: :delete}, class: "float-end btn btn-xs btn-outline-danger")
+    end
+  end
+
   def header_edit_button(resource)
     return unless resource.present?
     if policy(resource).edit?

f21daf3a670a42236ea5dd5e8e662af1eea72ca7
David Singer <david@ramaboo.com>
2022-02-13 01:58:58 -0800

    redis config

--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -8,10 +8,12 @@ module Admin
 
     # GET /admin/system
     def system
-      flash.now[:success] = "This is a <b>success</b>.".html_safe
-      flash.now[:notice] = "This is a <b>notice</b>.".html_safe
-      flash.now[:warning] = "This is a <b>warning</b>.".html_safe
-      flash.now[:alert] = "This is an <b>alert</b>.".html_safe
+      if Rails.env.development?
+        flash.now[:success] = "This is a <b>success</b>.".html_safe
+        flash.now[:notice] = "This is a <b>notice</b>.".html_safe
+        flash.now[:warning] = "This is a <b>warning</b>.".html_safe
+        flash.now[:alert] = "This is an <b>alert</b>.".html_safe
+      end
     end
 
     private

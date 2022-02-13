eac218b80d57d12c6f2a94c3e2b33bf66d04970a
David Singer <david@ramaboo.com>
2022-02-13 02:20:19 -0800

    save

--- app/helpers/card_helper.rb
+++ app/helpers/card_helper.rb
@@ -1,17 +1,18 @@
 module CardHelper
-  def icon_card_header(icon, text, button: nil, pagy: nil, class_name: nil)
+  def card_header(icon, text, button: nil, pagy: nil, class_name: nil)
     tag.div(class: "card-header #{class_name}".strip) do
       capture do
         concat bootstrap_icon(icon)
         concat tag.span(text, class: "ms-1 me-1")
         concat pagy_count_badge(pagy) if pagy.present?
         concat button if button.present?
+        yield if block_given?
       end
     end
   end
 
   def error_card_header(error)
-    icon_card_header("exclamation-octagon", " Error #{error}", class_name: "bg-danger text-white")
+    card_header("exclamation-octagon", " Error #{error}", class_name: "bg-danger text-white")
   end
 
   def pagy_card_footer(pagy)

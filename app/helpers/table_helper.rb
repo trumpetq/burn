99d87223337a46598fc60a611af716a9b0a9a04e
David Singer <david@ramaboo.com>
2022-02-12 22:23:36 -0800

    Starting turbo frame work

--- app/helpers/table_helper.rb
+++ app/helpers/table_helper.rb
@@ -1,4 +1,8 @@
 module TableHelper
+  def admin_table_sort(column:, label:)
+    link_to(label, admin_users_path(column: column))
+  end
+
   def table_row_class(resource)
     return unless resource.present?
     if resource.try(:discarded?)

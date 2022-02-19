25ba3455189510e95b0c4d92293173f209949021
David Singer <david@ramaboo.com>
2022-02-19 01:43:59 -0800

    adding status to admin

--- app/controllers/admin/camp_applications_controller.rb
+++ app/controllers/admin/camp_applications_controller.rb
@@ -3,14 +3,21 @@ module Admin
     include Admin::Campable
 
     def approve_after_save
+      return unless @resource.user.present?
+
       CampApplicationMailer.with(user: @resource.user).approve_email.deliver_now if send_email
     end
 
     def complete_after_save
+      return unless @resource.user.present?
+
       CampApplicationMailer.with(user: @resource.user).complete_email.deliver_now if send_email
+      @resource.user.update(plan: :camping_with_us)
     end
 
     def reject_after_save
+      return unless @resource.user.present?
+
       CampApplicationMailer.with(user: @resource.user).reject_email.deliver_now if send_email
     end
   end

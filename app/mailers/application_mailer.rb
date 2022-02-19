8524edcfbf21167cd413b0d58d008e37de810e67
David Singer <david@ramaboo.com>
2022-02-18 17:40:59 -0800

    better eamil

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: Settings.email.contact
+  default from: Settings.email.from
   layout "mailer"
   prepend_view_path(Rails.root.join("app/views/mailers"))
 end

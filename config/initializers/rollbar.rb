b1ff305e0895d9c203f805c6c30b70a9fafac0ff
David Singer <david@ramaboo.com>
2022-05-27 22:06:42 -0700

    rollbar live

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -1,9 +1,18 @@
+rollbar_handler = proc do |options|
+  case options[:exception]
+  when ActionController::RoutingError
+    needles = ["wp-admin", "wp-includes", "xmlrpc.php"]
+    raise Rollbar::Ignore if options[:message]&.match?(Regexp.union(needles))
+  end
+end
+
 Rollbar.configure do |config|
   config.access_token = ENV["ROLLBAR_ACCESS_TOKEN"]
 
   if Rails.env.test?
     config.enabled = false
   end
+  config.before_process << rollbar_handler
 
   config.environment = ENV["ROLLBAR_ENV"].presence || Rails.env
 end

2e83b2e0c9bd40de449f9e04af56562772184bdf
David Singer <david@ramaboo.com>
2022-07-29 17:59:31 -0700

    new form fix

--- app/policies/admin/nil_class_policy.rb
+++ app/policies/admin/nil_class_policy.rb
@@ -1,6 +1,26 @@
- module Admin
+module Admin
   class NilClassPolicy < ApplicationPolicy
+    def index?
+      false?
+    end
+
     def show?
+      false
+    end
+
+    def edit?
+      false
+    end
+
+    def create?
+      false
+    end
+
+    def update?
+      false
+    end
+
+    def destroy?
       false
     end
   end

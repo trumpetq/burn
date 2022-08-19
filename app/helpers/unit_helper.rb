04ec885bea402ac5fe589dff93bc447bb30adea3
David Singer <david@ramaboo.com>
2022-08-19 00:21:47 -0700

    save

--- app/helpers/unit_helper.rb
+++ app/helpers/unit_helper.rb
@@ -4,4 +4,10 @@ module UnitHelper
 
     "#{number} ft"
   end
+
+  def size_in_feet(resource)
+    return na_badge if resource.length.blank? && resource.width.blank?
+
+    "#{in_feet(resource.length.to_f)} x #{in_feet(resource.width.to_f)}"
+  end
 end
